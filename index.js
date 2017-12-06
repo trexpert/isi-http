c6495944632085d2d609a037375027e155e0b24e
Benjamin Isberner <b.isberner@sensory-minds.com>
2017-12-06 17:03:03 +0100

    fix post data

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var
 		http  = require( 'http' ),
 		https = require( 'https' ),
 		Q     = require( 'q' ),
-		url   = require( 'url' );
+		url   = require( 'url' ),
+		util  = require( 'util' );
 
 var requestOptions = {
 	host    : undefined,
@@ -32,10 +33,33 @@ var interface = {
 		return this.request( options );
 	},
 
+	_stringifyParameters : function ( dataToSend ) {
+		if ( dataToSend === undefined ) {
+			return undefined;
+		}
+
+		var queryStrings = [];
+		for ( var key in dataToSend ) {
+			queryStrings.push( util.format( "%s=%s", key, dataToSend[ key ] ) );
+		}
+
+		return queryStrings.join( '&' );
+	},
+
 	request : function ( options ) {
-		var deferred      = Q.defer();
-		var usedLib       = options.port == 443 ? https : http;
-		var data          = options.data == undefined ? "" : JSON.stringify( options.data );
+		var deferred = Q.defer();
+		var usedLib  = options.port == 443 ? https : http;
+		var data     = "";
+
+		if ( options.data !== undefined ) {
+			if ( options.format === 'json' ) {
+				data = JSON.stringify( options.data );
+			} else {
+				data = this._stringifyParameters( options.data )
+				// data = JSON.stringify( options.data );
+			}
+		}
+
 		var writeToStream = false;
 
 		if ( ( options.method == "PUT" || options.method == "DELETE" || options.method == "POST") && options.data != undefined ) {
@@ -43,9 +67,16 @@ var interface = {
 				options.headers = {}
 			}
 
-			options.headers[ 'Content-Type' ]   = 'application/json';
+			if ( options.format === 'json' ) {
+				options.headers[ 'Content-Type' ] = 'application/json';
+			} else {
+				options.headers[ 'Content-Type' ] = 'application/x-www-form-urlencoded';
+				// options.body                      = data;
+			}
+
 			options.headers[ 'Content-Length' ] = Buffer.byteLength( data );
 			delete options.data;
+			delete options.format;
 			writeToStream = true;
 		}
 
@@ -65,7 +96,7 @@ var interface = {
 					obj = JSON.parse( output );
 				}
 
-				deferred.resolve( { status : res.statusCode, data : obj } );
+				deferred.resolve( { status : res.statusCode, data : obj, raw : output } );
 			} );
 		} );
 
